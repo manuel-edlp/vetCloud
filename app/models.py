b9b76e4cc4a43008e63576fcc2a2f0c652addfb1
Ivan D'Uva <duva.ivan@outlook.com>
2024-04-20 15:44:32 -0300

    Crea modelo Cliente

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=15)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
