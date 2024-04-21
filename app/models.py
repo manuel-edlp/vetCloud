8b04c2e6a1452b1802d5c7f1d8293190c586eaca
Rodrigo Jacznik <rodrigo.jacznik@gmail.com>
2024-04-21 18:28:46 -0300

    Pasa codigo del modelo a ingles. Agrega direccion

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-class Cliente(models.Model):
-    nombre = models.CharField(max_length=100)
-    telefono = models.CharField(max_length=15)
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
     email = models.EmailField()
+    address = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
-        return self.nombre
+        return self.name
