6332174ec52daa86eca0c82f015c6f05dd7a85c1
GonzaloEBaez <gezbaez@gmail.com>
2024-04-27 23:59:06 -0300

    Agregando URLs

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("clientes/nuevo/", view=views.clients_form, name="clients_form"),
     path("clientes/editar/<int:id>/", view=views.clients_form, name="clients_edit"),
     path("clientes/eliminar/", view=views.clients_delete, name="clients_delete"),
+    path("proveedor/", view=views.provider_repository, name="provider_repo"),
+    path("proveedor/nuevo/", view=views.provider_form, name="provider_form"),
+    path("proveedor/editar/<int:id>/", view=views.provider_form, name="provider_edit"),
+    path("proveedor/eliminar/", view=views.provider_delete, name="provider_delete"),
 ]
