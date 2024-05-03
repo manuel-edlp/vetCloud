7c58a552c40f4aed55c7e823c15c94a8a98457c2
Manuel Morullo <manuelmorullo@hotmail.com>
2024-05-02 22:33:10 -0300

    Arreglo de traduccion del NavBar

--- app/context_processors.py
+++ app/context_processors.py
@@ -1,7 +1,7 @@
 from django.urls import reverse
 
 links = [
-    {"label": "Home", "href": reverse("home"), "icon": "bi bi-house-door"},
+    {"label": "Inicio", "href": reverse("home"), "icon": "bi bi-house-door"},
     {"label": "Clientes", "href": reverse("clients_repo"), "icon": "bi bi-people"},
     {"label": "Proveedores", "href": reverse("provider_repo"), "icon": "bi bi-people"},
     {"label": "Productos", "href": reverse("product_repo"), "icon": "bi bi-box"},
