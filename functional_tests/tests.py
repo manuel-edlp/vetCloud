89e80fd24fbc0f6dee6917114a0d841672361a9c
Feliciano <felygl77@hotmail.com>
2024-05-26 18:44:25 -0300

    Agreg test end-to-end para la clase Medicine

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,7 +5,7 @@ from playwright.sync_api import sync_playwright, expect, Browser
 
 from django.urls import reverse
 
-from app.models import Client
+from app.models import Client, Medicine
 
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 playwright = sync_playwright().start()
@@ -241,4 +241,72 @@ class ClientCreateEditTestCase(PlaywrightTestCase):
         edit_action = self.page.get_by_role("link", name="Editar")
         expect(edit_action).to_have_attribute(
             "href", reverse("clients_edit", kwargs={"id": client.id})
+        )
+
+class MedicineCreateEditTestCase(PlaywrightTestCase):
+    def test_should_be_able_to_create_a_new_medicine(self):
+        self.page.goto(f"{self.live_server_url}{reverse('medicine_form')}")
+
+        expect(self.page.get_by_role("form")).to_be_visible()
+
+        self.page.get_by_label("Nombre").fill("Medicina A")
+        self.page.get_by_label("Dosis").fill("5")
+        self.page.get_by_label("Precio").fill("10.50")
+
+        self.page.get_by_role("button", name="Guardar").click()
+
+        expect(self.page.get_by_text("Medicina A")).to_be_visible()
+        expect(self.page.get_by_text("5")).to_be_visible()
+        expect(self.page.get_by_text("10.50")).to_be_visible()
+
+    def test_should_view_errors_if_form_is_invalid(self):
+        self.page.goto(f"{self.live_server_url}{reverse('medicine_form')}")
+
+        expect(self.page.get_by_role("form")).to_be_visible()
+
+        self.page.get_by_role("button", name="Guardar").click()
+
+        expect(self.page.get_by_text("Por favor ingrese un nombre")).to_be_visible()
+        expect(self.page.get_by_text("Por favor ingrese una dosis")).to_be_visible()
+        expect(self.page.get_by_text("Por favor ingrese un precio")).to_be_visible()
+
+        self.page.get_by_label("Nombre").fill("Medicina A")
+        self.page.get_by_label("Dosis").fill("0")
+        self.page.get_by_label("Precio").fill("10.50")
+
+        self.page.get_by_role("button", name="Guardar").click()
+
+        expect(self.page.get_by_text("Por favor ingrese un nombre")).not_to_be_visible()
+        expect(self.page.get_by_text("Por favor ingrese una dosis")).not_to_be_visible()
+        expect(self.page.get_by_text("Por favor ingrese un precio")).not_to_be_visible()
+
+        expect(self.page.get_by_text("Por favor ingrese una dosis válida")).to_be_visible()
+
+    def test_should_be_able_to_edit_a_medicine(self):
+        medicine = Medicine.objects.create(
+            name="Medicina A",
+            dose=5,
+            price=10.50,
+        )
+
+        path = reverse("medicine_edit", kwargs={"id": medicine.id})
+        self.page.goto(f"{self.live_server_url}{path}")
+
+        self.page.get_by_label("Nombre").fill("Medicina B")
+        self.page.get_by_label("Dosis").fill("10")
+        self.page.get_by_label("Precio").fill("15.75")
+
+        self.page.get_by_role("button", name="Guardar").click()
+
+        expect(self.page.get_by_text("Medicina A")).not_to_be_visible()
+        expect(self.page.get_by_text("5")).not_to_be_visible()
+        expect(self.page.get_by_text("10.50")).not_to_be_visible()
+
+        expect(self.page.get_by_text("Medicina B")).to_be_visible()
+        expect(self.page.get_by_text("10")).to_be_visible()
+        expect(self.page.get_by_text("15.75")).to_be_visible()
+
+        edit_action = self.page.get_by_role("link", name="Editar")
+        expect(edit_action).to_have_attribute(
+            "href", reverse("medicine_edit", kwargs={"id": medicine.id})
         )
