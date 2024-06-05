50c79dc80936f513a0c5c5c318e5bb0b553a41ea
Manuel Morullo <manuelmorullo@hotmail.com>
2024-06-04 23:02:58 -0300

    Agrego validacion de ciudad

--- app/models.py
+++ app/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from datetime import datetime
 
-
+class CityEnum(models.TextChoices):
+    LA_PLATA = 'La Plata',
+    BERISSO = 'Berisso',
+    ENSENADA = 'Ensenada',
 
 def validate_client(data):
     errors = {}
@@ -9,6 +12,7 @@ def validate_client(data):
     name = data.get("name", "")
     phone = data.get("phone", "")
     email = data.get("email", "")
+    city = data.get("city", "")
 
     if name == "":
         errors["name"] = "Por favor ingrese un nombre"
@@ -21,6 +25,11 @@ def validate_client(data):
     elif email.count("@") == 0:
         errors["email"] = "Por favor ingrese un email valido"
 
+    if city == "" or city is None:
+        errors["city"] = "Por favor ingrese una ciudad"
+    elif city not in dict(CityEnum.choices):
+        errors["city"] = "Por favor ingrese una ciudad valida"
+
     return errors
 
 
@@ -28,7 +37,7 @@ class Client(models.Model):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
     email = models.EmailField()
-    city = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=35, choices=CityEnum.choices)
 
     def __str__(self):
         return self.name
@@ -50,12 +59,19 @@ class Client(models.Model):
         return True, None
 
     def update_client(self, client_data):
+        errors = validate_client(client_data)
+
+        if len(errors.keys()) > 0:
+            return False, errors
+
         self.name = client_data.get("name", "") or self.name
         self.email = client_data.get("email", "") or self.email
         self.phone = client_data.get("phone", "") or self.phone
         self.city = client_data.get("city", "") or self.city
 
         self.save()
+
+        return True, None
 
 def validate_provider(data):
     errors = {}
