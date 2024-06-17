4a3e0e10e41523afc24c0a1c076615381144b84a
Manuel Morullo <manuelmorullo@hotmail.com>
2024-06-17 01:31:07 -0300

    actualizo iconos

--- app/context_processors.py
+++ app/context_processors.py
@@ -4,11 +4,11 @@ from django.urls import reverse
 links = [
     {"label": "Inicio", "href": reverse("home"), "icon": "bi bi-house-door"},
     {"label": "Clientes", "href": reverse("clients_repo"), "icon": "bi bi-people"},
-    {"label": "Proveedores", "href": reverse("provider_repo"), "icon": "bi bi-people"},
-    {"label": "Productos", "href": reverse("product_repo"), "icon": "bi bi-box"},
-    {"label": "Mascotas", "href": reverse("pet_repo"), "icon": "bi bi-people"},
-    {"label": "Medicamentos", "href": reverse("medicine_repo"), "icon": "bi bi-people"},
-    {"label": "Veterinarios", "href": reverse("veterinary_repo"), "icon": "bi bi-people"},
+    {"label": "Proveedores", "href": reverse("provider_repo"), "icon": "bi bi-briefcase"},
+    {"label": "Productos", "href": reverse("product_repo"), "icon": "bi bi-box-seam"},
+    {"label": "Mascotas", "href": reverse("pet_repo"), "icon": "bi bi-piggy-bank"},
+    {"label": "Medicamentos", "href": reverse("medicine_repo"), "icon": "bi bi-capsule"},
+    {"label": "Veterinarios", "href": reverse("veterinary_repo"), "icon": "bi bi-person-hearts"},
 ]
 
 def navbar(request):
