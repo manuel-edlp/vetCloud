98997b1e0f5bb56c8b661d5959402593c8e791e1
Manuel Morullo <manuelmorullo@hotmail.com>
2024-06-19 19:55:18 -0300

    arreglo csrf

--- vetsoft/settings.py
+++ vetsoft/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['vet-cloud.azurewebsites.net','127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['https://vet-cloud.azurewebsites.net/']
+CSRF_TRUSTED_ORIGINS = ['https://vet-cloud.azurewebsites.net']
 
 # Application definition
 
